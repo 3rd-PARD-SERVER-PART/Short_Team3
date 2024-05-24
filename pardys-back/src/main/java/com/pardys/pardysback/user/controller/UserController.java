6bb667104534d247da139280028660ec29df7a66
Whal3 <leehan416@gmail.com>
2024-05-25 00:32:55 +0900

    [add] add file
    user 구현중

---
+++ pardys-back/src/main/java/com/pardys/pardysback/user/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.pardys.pardysback.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+    @PostMapping
+
+
+}
