6aea65fdb75cb73d9cc75cea9100664e60331b71
Whal3 <leehan416@gmail.com>
2024-05-24 23:49:40 +0900

    [add] test page
    test 완료

---
+++ pardys-back/src/main/java/com/pardys/pardysback/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.pardys.pardysback.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                // Origin이 http:localhost:3000에 대해
+                .allowedOrigins("http://localhost:3000")
+                // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 허용한다.
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
