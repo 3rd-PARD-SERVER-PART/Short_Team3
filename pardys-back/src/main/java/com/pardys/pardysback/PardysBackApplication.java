d5b108d8b6a4357dff8311dc7d233ca78c5443f7
Junhyung Lee <joseph3.lee@gmail.com>
2024-05-24 23:21:34 +0900

    [add] project file
    upload

---
+++ pardys-back/src/main/java/com/pardys/pardysback/PardysBackApplication.java
@@ -0,0 +1,13 @@
+package com.pardys.pardysback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PardysBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PardysBackApplication.class, args);
+    }
+
+}
---
+++ pardys-back/src/test/java/com/pardys/pardysback/PardysBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pardys.pardysback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PardysBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
